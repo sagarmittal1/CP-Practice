af5bd34ea7cd0ee45e7f9bd6f5c8452be595542b
ARPIT GUPTA <ARPITGUPTA1014@gmail.com>
2021-10-01 22:14:35 +0530

    Adding solution of Mancunian And Colored Tree

--- HackerEarth/Data Structures/Mancunian And Colored Tree.cpp
+++ HackerEarth/Data Structures/Mancunian And Colored Tree.cpp
@@ -1,38 +1,79 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+#define ll long long
+
 using namespace std;
+
  
-int color[100001],parent[100001];
-int main()
+
+void dfs(int u, vector<vector<int>> &adj, vector<int> &colors, vector<int> &ans, vector<stack<int>> &stk)
+
 {
- int n,c,i,flag,x;
- cin>>n>>c;
- for(i=2;i<=n;i++)
- {
- cin>>x;
- parent[i]=x;
- }
- for(i=1;i<=n;i++)
- {
- cin>>color[i];
- }
+
+if(stk[colors[u]].empty())
+
+ ans[u] = -1;
+
+else
+
+ ans[u] = stk[colors[u]].top();
+
  
- for(i=1;i<=n;i++)
- {
- flag=0;
- int p=i;
- while(p!=1)
- {
- p=parent[p];
- if(color[i]==color[p])
- {
- cout<<p<<" ";
- flag=1;
- break;
- }
- }
- if(flag==0)
- cout<<"-1 ";
- 
- }
- 
+
+stk[colors[u]].push(u);
+
+for(auto v:adj[u])
+
+{
+
+ dfs(v, adj, colors, ans, stk);
+
+}
+
+stk[colors[u]].pop();
+
+}
+
+int main()
+
+{
+
+   ios_base::sync_with_stdio(false);
+
+   cin.tie(NULL);
+
+   int n, c;
+
+   cin>>n>>c;
+
+   vector<vector<int>> adj(n+1);
+
+   vector<int> colors(n+1);
+
+   vector<int> ans(n+1, 0);
+
+   vector<stack<int>> stk(n+1);
+
+   for(int i=1; i<=n; ++i)
+
+   {
+
+    int p;
+
+    cin>>p;
+
+    adj[p].push_back(i);
+
+   }
+
+   for(int i=1; i<=n; ++i)
+
+    cin>>colors[i];
+
+   dfs(1, adj, colors, ans, stk);
+
+   for(int i=1; i<=n; ++i)
+
+    cout<<ans[i]<<" ";
+
 }
