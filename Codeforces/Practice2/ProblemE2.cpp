1f0cb2312795caf6bbedbe2cd78fba4282af3808
Nishkarsh Sharma <63001768+nishkarsh540@users.noreply.github.com>
2021-10-01 23:01:09 +0530

    Problem E2 Solution  (#20)
    * Issue 327A solved
    
    * Issue 327A solved
    
    * Issue 327A solved
    
    * Problem E2 solved
    
    * Delete Flipping.cpp
    
    * Delete Flipping.cpp
    
    * Update ProblemE2.cpp

---
+++ Codeforces/Practice2/ProblemE2.cpp
@@ -0,0 +1,41 @@
+//https://codeforces.com/problemset/problem/1579/E2
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+ 
+ 
+using namespace std;
+using namespace __gnu_pbds;
+typedef pair<int, int> node;
+typedef tree<node, null_type, less<node>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+ 
+int n,m,k;
+ 
+void solve()
+{
+	cin>>n;
+	ordered_set os;
+	long long int ans =0;
+	for(int i =0;i<n;i++){
+		int t;
+		cin>>t;
+		os.insert(node(t,i));
+		int x = os.order_of_key(node(t,0));
+		int same = os.order_of_key(node(t,i))-x;
+		int y = i-same-x;
+		ans = ans + min(x,y);
+	}
+	cout<<ans;
+		
+}
+int main()
+{
+	int T;
+	cin >> T;
+	while(T--){
+		solve();
+		cout<<"\n";
+	}
+	return 0;
+} 
+ 
+ 
