f6d690d4181571b10807967077c320dc2f4509a1
Nishkarsh Sharma <nishkarshsharma540@gmail.com>
2021-10-01 21:23:34 +0530

    Solved the issue.

---
+++ Codeforces/PRACTICE/Untitled-1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,a,c,x,m,r=-1;
+int main()
+{
+	cin>>n;
+	while(n--)
+	{
+		cin>>a;
+		c=c+a;
+		x=x+1-a*2;
+		r=max(r,x-m);
+		m=min(x,m);
+	}
+	cout<<r+c;
+	return 0;
+}
